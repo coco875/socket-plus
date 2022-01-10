4ea545614ea1e287208a2053ba3bb6f7ec1cfd8e
coco875 <59367621+coco875@users.noreply.github.com>
2022-01-10 14:29:33 +0100

    b

--- socket_game/main.py
+++ socket_game/main.py
@@ -2,60 +2,64 @@ import socket
 import threading
 
 class Client_connection:
-    def __init__(self, ip:str, port:int, header:list, format:list):
+    def __init__(self, adresse_ip:str, port:int, header:list, format_socket:list):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.header = header
-        self.format = format
+        self.format = format_socket
         #self.sock.settimeout(0)
-        self.ip = ip
+        self.adresse_ip = adresse_ip
         self.port = port
-    
+
     def disconnect(self):
+        """disconnect client from server"""
         self.sock.close()
-    
+
     def connect(self):
-        self.sock.connect((self.ip, self.port))
+        """connect client from server"""
+        self.sock.connect((self.adresse_ip, self.port))
 
 class Server_connection(threading.Thread):
-    def __init__(self, port: int, header: list, format: list):
+    def __init__(self, adresse_ip: str, port: int, header: list, format_socket: list):
         threading.Thread.__init__(self)
         self.header = header
-        self.format = format
+        self.format = format_socket
+        self.adresse_ip = adresse_ip
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind(("localhost", port))
-    def run(self):
-        self.go = True
+        self.server.bind((self.adresse_ip, port))
+        self.starting = True
         self.all_thread = []
-        while self.go:
+    def run(self):
+        while self.starting:
             self.server.listen(1)
-            clientsock, clientAddress = self.server.accept()
+            clientsock, client_adress = self.server.accept()
             clientsock.setblocking(1)
-            newthread = ClientThread(clientAddress, clientsock)
+            newthread = ClientThread(client_adress, clientsock)
             newthread.start()
             self.all_thread.append(newthread)
     def stop(self):
-        self.go = False
+        """stop server"""
+        self.starting = False
         for i in self.all_thread:
             i.csocket.close()
         self.server.close()
 
 class ClientThread(threading.Thread):
-    def __init__(self, clientAddress, clientsocket):
+    def __init__(self, client_adress, clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
-        print("New connection added: ", clientAddress)
-        self.clientAddress = clientAddress
+        print("New connection added: ", client_adress)
+        self.client_adress = client_adress
 
     def run(self):
-        print("Connection from : ", self.clientAddress)
+        print("Connection from : ", self.client_adress)
         #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
         msg = ''
         while True:
             data = self.csocket.recv(2048)
             msg = data.decode()
             if msg == 'bye':
-              break
+                break
             print("from client", msg)
             self.csocket.send(bytes(msg, 'UTF-8'))
-        print("Client at ", clientAddress, " disconnected...")
+        print("Client at ", client_adress, " disconnected...")
--- test.py
+++ test.py
@@ -1,11 +1,32 @@
 import socket_game
 
-s = socket_game.Server_connection(8000)
+headers_socket = [
+    {
+        "name":"id_type",
+        "type":int,
+        "len":4
+    }
+]
+
+format_socket = {
+    0:[
+        {
+            "name":"len_msg",
+            "type":int,
+            "len":4
+        },
+        {
+            "name":"msg",
+            "type":str,
+            "len":"len_msg"
+        }
+    ]
+}
+
+s = socket_game.Server_connection("localhost", 8000, headers_socket, format_socket)
 s.start()
-c = socket_game.Client_connection("localhost", 8000)
-input()
+c = socket_game.Client_connection("localhost", 8000,  headers_socket, format_socket)
 c.connect()
 input()
 c.disconnect()
 s.stop()
-
